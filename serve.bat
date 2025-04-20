a64442a654838f8caa0fbf4e8b189c139ab45469
Remy Blank <remy@c-space.org>
2025-04-20 11:51:46 +0200

    Update run.py.

--- serve.bat
+++ serve.bat
@@ -1,6 +1,6 @@
 @echo off
 echo The serve.bat launcher is deprecated. Please use run.py instead,
 echo and update your shortcuts.
-echo See ^<https://t-doc.org/common/edit.html#edit-documents^>.
+echo See ^<https://common.t-doc.org/edit.html#edit-documents^>.
 echo.
 pause
