1b96fe8300a1a0bec38ed5d7c4c3480db6018dc4
Remy Blank <remy@c-space.org>
2025-06-28 16:20:48 +0200

    Remove the repository menu.

--- docs/conf.py
+++ docs/conf.py
@@ -11,5 +11,5 @@ language = 'fr'
 myst_links_external_new_tab = True
 
 html_theme_options = {
-    'repository_url': 'https://github.com/t-doc-org/janm',
+    # 'repository_url': 'https://github.com/t-doc-org/janm',
 }
